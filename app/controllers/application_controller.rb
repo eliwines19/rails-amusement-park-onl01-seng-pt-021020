5fe8d64ae29d356bacfe5e5f4498ab4dd397c574
Elijah wines <elijahwines84@yahoo.com>
2020-07-14 21:08:22 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,25 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_methods :logged_in?, :current_user, :admin_only
-
-  def logged_in?
-    !!current_user
-  end
+  helper_method :current_user, :logged_in?, :admin_only
 
   def current_user
     current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
 
+  def logged_in?
+    !!current_user
+  end
+
   def admin_only
     if !current_user.admin
-      redirect_to root_path, notice: "You do not have permission to view this page"
+      redirect_to root_path, notice: "You do not have permission to view this page."
     end
   end
 
   private
-
-  def require_login
-    redirect_to root_path unless session.include? :user_id
-  end
-
+    def require_login
+      redirect_to root_path unless session.include? :user_id
+    end
 end
