638b7b904167fadc9cb97a50a9603067dd552637
Elijah wines <elijahwines84@yahoo.com>
2020-07-14 21:08:56 +0000

    Automatically backed up by Learn

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,4 +1,5 @@
 class RidesController < ApplicationController
+  before_action :set_ride, only: [:show, :edit, :update, :destroy]
 
   def index
     @rides = Ride.all
@@ -11,27 +12,27 @@ class RidesController < ApplicationController
     @ride = Ride.new
   end
 
+  def edit
+  end
+
   def create
     @ride = Ride.new(user_id: current_user.id, attraction_id: params[:attraction_id])
     if @ride.save
       if @ride.user.admin
         redirect_to attraction_path(@ride.attraction)
       else
-        redirect_to user_path(current_user), notice: "@ride.take_ride"
+        redirect_to user_path(current_user), notice: @ride.take_ride
       end
     else
       redirect_to attraction_path(attraction), notice: "#{@ride.attraction.name} was not ridden."
     end
   end
 
-  def edit
-  end
-
   def update
     respond_to do |format|
-      if @ride.update.(ride_params)
+      if @ride.update(ride_params)
         format.html { redirect_to @ride, notice: 'Ride was successfully updated.' }
-        format.json { render :show, status: ok, location: @ride }
+        format.json { render :show, status: :ok, location: @ride }
       else
         format.html { render :edit }
         format.json { render json: @ride.errors, status: :unprocessable_entity }
@@ -42,19 +43,17 @@ class RidesController < ApplicationController
   def destroy
     @ride.destroy
     respond_to do |format|
-      format.html { redirect_to rides_url, notice: "Ride was successfully destroyed."}
-      format.json { head :no_content}
+      format.html { redirect_to rides_url, notice: 'Ride was successfully destroyed.' }
+      format.json { head :no_content }
     end
   end
 
   private
+    def set_ride
+      @ride = Ride.find_by(id: params[:id])
+    end
 
-  def ride_params
-    params.fetch(:ride {})
-  end
-
-  def set_ride
-    @ride = Ride.find_by(id: params[:id])
-  end
-  
+    def ride_params
+      params.fetch(:ride, {})
+    end
 end
