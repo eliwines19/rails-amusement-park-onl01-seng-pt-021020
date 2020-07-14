d64c47c3ed44522f0f9a520a518be6e450cf2c8b
Elijah wines <elijahwines84@yahoo.com>
2020-07-14 21:08:40 +0000

    Automatically backed up by Learn

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,25 +1,22 @@
 class AttractionsController < ApplicationController
+
   def index
     @attractions = Attraction.all
+  end
+
+  def show
+    @attraction = Attraction.find_by(id: params[:id])
+    @ride = @attraction.rides.build(user_id: current_user.id)
   end
 
   def new
     @attraction = Attraction.new
   end
 
-  def show
-    @attraction = Attraction.find_by(id: params[:id])
-    @ride = Attraction.rides.build(user_id: current_user.id)
-  end
-
   def create
     attraction = Attraction.create(attraction_params)
-    redirect_to attraction_path(attraction)
-  end
 
-  def update
-    attraction = Attraction.find_by(id: params[:id])
-    attraction.update(attraction_params)
+
     redirect_to attraction_path(attraction)
   end
 
@@ -28,10 +25,16 @@ class AttractionsController < ApplicationController
     @ride = @attraction.rides.build(user_id: current_user.id)
   end
 
+  def update
+    attraction = Attraction.find_by(id: params[:id])
+    attraction.update(attraction_params)
+    redirect_to attraction_path(attraction)
+  end
+
   private
 
-  def attraction_params
-    params.require(:attraction).permit(:name, :min_height, :tickets, :happiness_rating, :nausea_rating)
-  end
+    def attraction_params
+      params.require(:attraction).permit(:name, :min_height, :tickets, :happiness_rating, :nausea_ratiing)
+    end
 
 end
